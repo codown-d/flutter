b90f492e58bb60b6f82038d583d8551f24676ba1
DESKTOP-SQL335Q\zhanghe <123456@qq.com>
2022-03-08 17:57:10 +0800

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
+import 'Form.dart';
 import 'RandomWords.dart';
 import 'TapboxA.dart';
+//import 'TapboxB.dart';
+import 'TapboxC.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() {runApp(const MyApp());}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  static const String _title = 'Flutter Code Sample';
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      //home: RandomWords(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
     );
   }
 }
